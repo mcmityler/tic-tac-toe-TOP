092e353dd108df0363a687f1fb32d2278d277d2d
Tyler McMillan <36271467+mcmityler@users.noreply.github.com>
2026-06-12 13:34:56 -0400

    Add basic javascript logic for gameboard

--- script.js
+++ script.js
@@ -1,41 +1,52 @@
-function createPlayer(name, emoji, symbol){
+function createPlayer(name, emoji){
     //create a player object here
     let myScore = 0;
-    
-    return {
-        getName(){
-            return name;
-        },
-        getEmoji(){
-            return emoji;
-        },
-        getSymbol(){
-            return symbol;
-        },
-        addScore(){
-            myScore++;
-        },
-        getScore(){
-            return myScore
-        }
+    function getName(){
+        return name;
     }
+    function getEmoji(){
+        return emoji;
+    }
+    function addScore(){
+        myScore++;
+    }
+    function getScore(){
+        return myScore;
+    }
+    return {getName, getEmoji, getScore, addScore};
 
 }
 
 const gameBoard = (()=>{
-    //create the new game board that stores board info & funcs
-    const boardSquare =
-    [["", "", ""],
-    ["", "", ""],
-    ["", "", ""]];
+    const _boardTiles = new Array(9); //references to the game board tiles
+    /* Gameboard tiles
+        [0,1,2]
+        [3,4,5]
+        [6,7,8]
+    */
 
-    function setSquare(arrX, arrY){
-
+    const _symbols = ["X","O"]; //if you are first you get X if you are second you get O
+    function resetTiles(){
+        for(let i = 0; i < _boardTiles.length; i++){
+            _boardTiles[i] = "";
+        }
+    }
+    function setTile(m_tileNum){
+        //if not filled in space then fill with current players symbol
+        if(_boardTiles[m_tileNum] === ""){
+            _boardTiles[m_tileNum] = _symbols[(gameManger.checkTurn()? 1 : 0)] //either returns a false or true
+            gameManger.changeTurns();
+        }
+        else{
+            console.log("Tile is already filled with: " + _boardTiles[m_tileNum]);
+        }
+        
     }
     function getBoard(){
-        return boardSquare;
+        return _boardTiles;
     }
-    return {setSquare, getBoard}
+    resetTiles(); //initialize tiles to empty strings
+    return {setTile, getBoard, resetTiles};
 })();
 
 const gameManger = (()=>{
@@ -49,18 +60,28 @@ const gameManger = (()=>{
 
     function checkTurn(){
         //check whos turn it currently is
-        if(isPlayer1Turn){
-            console.log("Is is player 1's turn");
-        }
-        else{
-            console.log("Is is player 2s's turn");
-        }
+        console.log(`Is is ${isPlayer1Turn ? "Player 1": "Player 2"}'s turn`);
         return isPlayer1Turn;
     }
 
-    return {changeTurns, checkTurn}
+    function playRound(){
+        //Randomly select whos first
+
+        //let first player select a square and fill with their symbol
+
+        //let next player select a square
+
+        //repeat taking turns until ...
+
+        //after 5 turns start checking win condition
+
+        //if someone wins add to their score
+        //exit the game loop and ask if they want to play again
+    }
+
+    return {changeTurns, checkTurn, playRound};
 
 })();
 
-const player1 = createPlayer("p1", "happy", "x");
-const player2 = createPlayer("p2", "mad", "o");
+const player1 = createPlayer("p1", "happy");
+const player2 = createPlayer("p2", "mad");
