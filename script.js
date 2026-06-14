7ed820ca6d27ceb3b0735a97fec1f61cb92d5ddf
Tyler McMillan <36271467+mcmityler@users.noreply.github.com>
2026-06-14 09:14:40 -0400

    Rework the array to be 2D also finish win check

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function createPlayer(name, emoji, symbol){
     let mySymbol = symbol;
     let myName = name;
     let myEmoji = emoji
+    let _isFirstPlayer = false;
     function setName(m_name){
         myName = m_name;
     }
@@ -28,30 +29,41 @@ function createPlayer(name, emoji, symbol){
     function getScore(){
         return myScore;
     }
-    return {getName, getEmoji, getScore, addScore, getSymbol, setSymbol};
+    function setFirstPlayer(m_isFirst){
+        _isFirstPlayer = m_isFirst;
+    }
+    function getFirstPlayer(){
+        return _isFirstPlayer;
+    }
+    return {getName, getEmoji, getScore, addScore, getSymbol, setSymbol, getFirstPlayer, setFirstPlayer};
 
 }
 
 const gameBoard = (()=>{
-    const _boardTiles = new Array(9); //references to the game board tiles
-    /* Gameboard tiles
-        [0,1,2]
-        [3,4,5]
-        [6,7,8]
-    */
+    const _boardTiles = [
+        ["","",""],
+        ["","",""],
+        ["","",""]
+    ]
     let _freeTileSpaces = 9;
 
-    
+
     function resetTiles(){
         for(let i = 0; i < _boardTiles.length; i++){
-            _boardTiles[i] = "";
+            for(let j = 0; j < _boardTiles[i].length; j++){
+                _boardTiles[i][j] = "";
+            }
         }
         _freeTileSpaces = 9;
     }
-    function setTile(m_tileNum){
+    function setTile(m_tileX, m_tileY){
+        if(gameManger.getWinner() > 0){
+            console.log("Game is already over");
+            return;
+        }
         //if not filled in space then fill with current players symbol
-        if(_boardTiles[m_tileNum] === ""){
-            _boardTiles[m_tileNum] = gameManger.checkTurn()? player1.getSymbol() : player2.getSymbol();//either returns a false or true
+        if(_boardTiles[m_tileX][m_tileY] === ""){
+            _boardTiles[m_tileX][m_tileY] = gameManger.checkTurn()? player1.getSymbol() : player2.getSymbol();//either returns a false or true
             gameManger.changeTurns();
             _freeTileSpaces -- ; // one less free tile space
             if(_freeTileSpaces < 5){
@@ -59,7 +71,7 @@ const gameBoard = (()=>{
             }
         }
         else{
-            console.log("Tile is already filled with: " + _boardTiles[m_tileNum]);
+            console.log("Tile is already filled with: " + _boardTiles[m_tileX][m_tileY]);
         }
         
     }
@@ -69,15 +81,38 @@ const gameBoard = (()=>{
     function checkBoardWin(){
         //check when 
         console.log("Check board winner");
-        // if(player1Won){
-        //     return 1;
-        // }
-        // else if(player2Won){
-        //     return 2;
-        // }
-        // else{
-        //     return 0; // no one one
-        // }
+        let m_winner = 0; // 0 - no one won ; 1 - player 1 ; 2 - player 2 
+        let m_winnerSymbol = "";
+        //0,0
+        if((_boardTiles[0][0] == _boardTiles[0][1] && _boardTiles[0][0] == _boardTiles[0][2]) //top side line
+        || (_boardTiles[0][0] == _boardTiles[1][0] && _boardTiles[0][0] == _boardTiles[2][0]) //left side line
+        ){
+            m_winnerSymbol = _boardTiles[0][0];
+        }
+        //1,1
+        else if((_boardTiles[1][1] == _boardTiles[0][0] && _boardTiles[1][1] == _boardTiles[2][2]) //top left to bottom right
+        || (_boardTiles[1][1] == _boardTiles[0][2] && _boardTiles[1][1] == _boardTiles[2][0]) //top right to bottom left
+        || (_boardTiles[1][1] == _boardTiles[1][0] && _boardTiles[1][1] == _boardTiles[1][2]) //middle left to middle right
+        || (_boardTiles[1][1] == _boardTiles[0][1] && _boardTiles[1][1] == _boardTiles[2][1])) //middle top to middle bottom
+        {
+            m_winnerSymbol = _boardTiles[1][1];
+        }
+        //2,2
+        else if((_boardTiles[2][2] == _boardTiles[1][2] && _boardTiles[2][2] == _boardTiles[0][2]) //right side line
+            ||(_boardTiles[2][2] == _boardTiles[2][1] && _boardTiles[2][2] == _boardTiles[2][0]) //bottom side line
+        ){
+            m_winnerSymbol = _boardTiles[2][2];
+        }
+
+
+        if(m_winnerSymbol != ""){
+            gameManger.setWinner(m_winnerSymbol === player1.getSymbol() ? 1 : 2);
+        }
+        else{
+            console.log("no winner yet")
+        }
+
+
     }
     resetTiles(); //initialize tiles to empty strings
     return {setTile, getBoard, resetTiles};
@@ -87,10 +122,12 @@ const gameManger = (()=>{
     //create a game manager that deals with game logic
     let isPlayer1Turn = false; // keeps track of whos turn it is
 
+    //keep track of game winner / if game is over
+    let gameWinner = 0; // 0 = no winner ; 1 = player 1 ; 2 = player 2
     function changeTurns(){
         //Swap whos turn it is
         isPlayer1Turn = !isPlayer1Turn;
-        console.log(`It is ${isPlayer1Turn === true ? "Player 1": "Player 2"}'s turn`);
+        console.log(`It is now ${isPlayer1Turn === true ? "Player 1": "Player 2"}'s turn`);
     }
 
     function checkTurn(){
@@ -98,20 +135,12 @@ const gameManger = (()=>{
         return isPlayer1Turn;
     }
 
-    function playRound(){
+    function startRound(){
         //Randomly select whos first 
         const randomInt = Math.floor(Math.random() * 2);
         player1.setSymbol(randomInt === 0 ? "X": "O");
         player2.setSymbol(randomInt === 0 ? "O": "X");
         isPlayer1Turn = (randomInt === 0); //if 0 player one goes first
-        if(randomInt === 0){
-            //player 1 goes first
-
-        }
-        else{
-            //player 2 goes first
-
-        }
         console.log(`it is ${isPlayer1Turn ? "Player 1" : "player 2"}'s turn to go first`);
         //let first player select a square and fill with their symbol
 
@@ -124,10 +153,18 @@ const gameManger = (()=>{
         //if someone wins add to their score
         //exit the game loop and ask if they want to play again
     }
-
-    return {changeTurns, checkTurn, playRound};
+    function setWinner(_winner){
+        gameWinner = _winner;
+        if(gameWinner === 1 || gameWinner === 2){
+            console.log(`${gameWinner === 1 ? player1.getName() : player2.getName()} won the game`);
+        }
+    }
+    function getWinner(){
+        return gameWinner;
+    }
+    return {changeTurns, checkTurn, startRound, setWinner, getWinner};
 
 })();
 
-const player1 = createPlayer("p1", "happy", "X");
+const player1 = createPlayer("tloko", "happy", "X");
 const player2 = createPlayer("p2", "mad", "O");
